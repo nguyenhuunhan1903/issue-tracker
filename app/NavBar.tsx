5b972cbc097cdc32c2f9dc70d9ac4fa0583824b6
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-09-29 16:22:47 +0700

    Add issues page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,20 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
 const NavBar = () => {
+    const currentPath=usePathname();
+    console.log(currentPath);
     const links=[
-        {label:'dashboard',link:'/dashboard'},
+        {label:'dashboard',link:'/'},
         {label:'issues',link:'/issues'},
     ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href={'/'}><AiFillBug/></Link>
         <ul className='flex space-x-6'>
-            {links.map((x)=><li key={x.link}><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={x.link}>{x.label}</Link></li>)}
+            {links.map((x)=><li key={x.link}><Link className={`${currentPath===x.link ?'text-zinc-900':'text-zinc-500' } hover:text-zinc-800 transition-colors`} href={x.link}>{x.label}</Link></li>)}
         </ul>
     </nav>
   )
