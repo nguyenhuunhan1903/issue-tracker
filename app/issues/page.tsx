28f2601a2c083e1056feacee66212c8aad223d81
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-07 09:24:11 +0700

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -24,7 +24,10 @@ const issues =async () => {
         <Table.Body>
           {issues.map(issue=>(
             <Table.Row key={issue.id}>
-                <Table.Cell>{issue.title}
+                <Table.Cell>
+                  <Link href={`/issues/${issue.id}`}>
+                  {issue.title}                  
+                  </Link>
                   <div className='block md:hidden'>
                     <IssueStatusBadge status={issue.status}/>
                   </div>
