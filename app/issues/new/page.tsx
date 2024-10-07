25464acaf324d1b7e560bfb3aa9ac5e279c4ccab
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-07 14:38:40 +0700

    Disable ssr on new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Button, Callout, Spinner, Text, TextArea, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react'
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -10,7 +9,9 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import dynamic from 'next/dynamic';
 type IssueForm = z.infer<typeof createIssueSchema>;
+const SimpleMDE=dynamic(()=>import('react-simplemde-editor'),{ssr:false});
 const NewIssuePage = () => {
     const route = useRouter();
     const [isSubmitting, setSubmit]=useState(false);
