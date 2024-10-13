48e38c7751363c093c117b54829fb7fd5e9efc8c
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-13 14:44:57 +0700

    Adding alertdialog on delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,32 @@
-import { Button } from '@radix-ui/themes'
+'use client';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
-interface Props{
-    issueId:Number
+interface Props {
+  issueId: Number
 }
 
-const DeleteIssueButton = ({issueId}:Props) => {
+const DeleteIssueButton = ({ issueId }: Props) => {
   return (
-    <Button color='red'>Delete Issue</Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you surely want to delete this issue?
+        </AlertDialog.Description>
+      <Flex mt="4" gap="3">
+        <AlertDialog.Cancel>
+          <Button variant='soft' color='gray'>Cancel</Button>
+        </AlertDialog.Cancel>
+        <AlertDialog.Action>
+          <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Action>
+      </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
