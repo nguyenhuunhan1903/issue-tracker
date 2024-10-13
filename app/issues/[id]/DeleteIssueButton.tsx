34b262d080821dda1c1fb91adc22baec1c9a7841
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-13 15:25:02 +0700

    Improve the user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex, Spinner } from '@radix-ui/themes'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
@@ -11,11 +11,13 @@ interface Props {
 const DeleteIssueButton = ({ issueId }: Props) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDelete,setIsDelete]=useState(false);
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color='red'>Delete Issue</Button>
+          <Button color='red' disabled={isDelete}>Delete Issue{isDelete&&<Spinner />}</Button>
+          
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -29,10 +31,12 @@ const DeleteIssueButton = ({ issueId }: Props) => {
             <AlertDialog.Action>
               <Button color='red' onClick={async () => {
                 try {
+                  setIsDelete(true);
                   await axios.delete('/api/issues/' + issueId);
                   router.push('/issues');
                   router.refresh();
                 } catch (error) {
+                  setIsDelete(false);
                   setError(true);
                 }
               }}>Delete Issue</Button>
