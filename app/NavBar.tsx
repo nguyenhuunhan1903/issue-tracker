66fe36b1c96fe0ffb70895e38ffb28b36a2d96b7
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-15 09:35:25 +0700

    Adding the login and Logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,13 @@
 'use client'
+import { Box } from '@radix-ui/themes'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
 const NavBar = () => {
     const currentPath=usePathname();
+    const {data:session,status}=useSession();
     const links=[
         {label:'dashboard',link:'/'},
         {label:'issues',link:'/issues'},
@@ -15,6 +18,14 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {links.map((x)=><li key={x.link}><Link className={`${currentPath===x.link ?'text-zinc-900':'text-zinc-500' } hover:text-zinc-800 transition-colors`} href={x.link}>{x.label}</Link></li>)}
         </ul>
+        <Box>
+          {status==='authenticated'&&<Link href="/api/auth/signout">
+            Log out
+          </Link>}
+          {status==='unauthenticated'&&(
+            <Link href="/api/auth/signin">Sign in</Link>
+          )}
+        </Box>
     </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from './NavBar'
 import "@radix-ui/themes/styles.css";
 import { Container, Theme } from "@radix-ui/themes";
 import './theme.config.css'
+import AuthProvider from './auth/Provider';
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
@@ -24,14 +25,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor='violet'>
-          <NavBar />
-          <main className='p-5'>
-            <Container>
-              {children}
-            </Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor='violet'>
+            <NavBar />
+            <main className='p-5'>
+              <Container>
+                {children}
+              </Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
