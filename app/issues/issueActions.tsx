2bbc7391eee02750346b628c056207198fff0ad0
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-19 15:38:46 +0700

    Building the filter component (still have error in AssigneeSelect)

--- app/issues/issueActions.tsx
+++ app/issues/issueActions.tsx
@@ -1,20 +1,17 @@
-'use client';
-import { Button, Flex } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { Button, Flex } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
 import IssueStatusFilter from './_components/IssueStatusFilter';
 
 const IssueActions = () => {
   return (
-    <Flex mb='5' justify='between'>
-        <IssueStatusFilter/>
-        <Button>
-        <Link href={'/issues/new'}>
-        Create new
-        </Link>
+    <Flex mb="5" justify="between">
+      <IssueStatusFilter />
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
       </Button>
     </Flex>
-  )
-}
+  );
+};
 
-export default IssueActions
+export default IssueActions;
