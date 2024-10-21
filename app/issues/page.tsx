f0f20c7674eb2ff0927099dfb21b0301d801ee4d
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-21 09:40:08 +0700

    Make table sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,61 +1,87 @@
-import prisma from '@/prisma/client'
-import { Table } from '@radix-ui/themes'
-import IssueStatusBadge from '../components/issueStatusBadge'
-import Link from '../components/Link'
-import IssueActions from './issueActions'
-import { Issue, Status } from '@prisma/client'
-const issues =async ({searchParams}:{searchParams:{status:any}}) => {
-//Ky thuat nay kha hay
-const statuses=Object.values(Status);
-const status=statuses.includes(searchParams.status)?searchParams.status:undefined;
-  let issues:Issue[]=[];
-  if(searchParams.status!=='NOSELECT'){
-  issues=await prisma.issue.findMany({
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import NextLink from 'next/link';
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import IssueActions from "./issueActions";
+import Link from "../components/Link";
+import IssueStatusBadge from "../components/issueStatusBadge";
+
+interface Props {
+  searchParams: { status: any, orderBy: keyof Issue };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    {
+      label: "Status",
+      value: "status",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Created",
+      value: "created",
+      className: "hidden md:table-cell",
+    },
+  ];
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
     where: {
-      status: status
-    }
-  });}
-  else
-  {
-    issues=await prisma.issue.findMany();
-  }
+      status,
+    },
+  });
+
   return (
     <div>
-      <div className='mb-5'>
-        <IssueActions/>
-      </div>
-      <Table.Root>
+      <IssueActions />
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                <NextLink href={{
+                  query: { ...searchParams, orderBy: column.value }                  
+                }}>{column.label}</NextLink>
+                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {issues.map(issue=>(
+          {issues.map((issue) => (
             <Table.Row key={issue.id}>
-                <Table.Cell>
-                  <Link href={`/issues/${issue.id}`}>
-                  {issue.title}                  
-                  </Link>
-                  <div className='block md:hidden'>
-                    <IssueStatusBadge status={issue.status}/>
-                  </div>
-                </Table.Cell>
-                <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status}/>
-                </Table.Cell>
-                <Table.Cell className='hidden md:table-cell'>{issue.created.toDateString()}</Table.Cell>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.created.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
     </div>
+  );
+};
 
-  )
-}
+export const dynamic = "force-dynamic";
 
-export const revalidate=0;
-
-export default issues
+export default IssuesPage;
