c54d477cd16164ce54f12c40b7a14974efef3fd3
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-25 16:17:58 +0700

    Build the issueSummary component

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -31,7 +31,7 @@ const IssueSummary = ({ open, inProgress, closed }: Props) => {
           <Flex direction="column" gap="1">
             <Link
               className='text-sm font-medium'
-              href={`/issues?status=${container.status}`}
+              href={`/issues/list?status=${container.status}`}
             >
               {container.label}
             </Link>
