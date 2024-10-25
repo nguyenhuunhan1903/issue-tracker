2ebc27594d1ba0ab28c05f67a5b6d1cfec3422fa
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-25 16:22:16 +0700

    Build the IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,18 @@
-import { Status } from '@prisma/client';
-import { Card, Flex, Text } from '@radix-ui/themes';
-import Link from 'next/link';
-import React from 'react';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
 
-interface Props {
-  open: number;
-  inProgress: number;
-  closed: number;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
+
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
-
-const IssueSummary = ({ open, inProgress, closed }: Props) => {
-  const containers: {
-    label: string;
-    value: number;
-    status: Status;
-  }[] = [
-    { label: 'Open Issues', value: open, status: 'OPEN' },
-    {
-      label: 'In-progress Issues',
-      value: inProgress,
-      status: 'IN_PROGRESS',
-    },
-    { label: 'Closed Issues', value: closed, status: 'CLOSED' },
-  ];
-
-  return (
-    <Flex gap="4">
-      {containers.map((container) => (
-        <Card key={container.label}>
-          <Flex direction="column" gap="1">
-            <Link
-              className='text-sm font-medium'
-              href={`/issues?status=${container.status}`}
-            >
-              {container.label}
-            </Link>
-            <Text size="5" className='font-bold'>{container.value}</Text>
-          </Flex>
-        </Card>
-      ))}
-    </Flex>
-  );
-};
-
-export default IssueSummary;
