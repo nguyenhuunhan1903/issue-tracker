891bdb94c58d6e4f738d8d19f8f891c7ccc8128d
Nhan Nguyen <nguyenhuunhan1903@gmail.com>
2024-10-28 09:36:10 +0700

    Add cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import prisma from '@/prisma/client'
-import React from 'react'
+import React, { cache } from 'react'
 import { notFound } from 'next/navigation';
 import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/issueStatusBadge';
@@ -14,11 +14,13 @@ interface Props {
     params: { id?: string }
 }
 
+const fetchUser=cache((issueId:number)=>prisma.issue.findUnique({
+    where:{id:issueId}
+}));
+
 const IssueDetailPage = async ({ params }: Props) => {
     const session = await getServerSession();
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id!) }
-    });
+    const issue = await fetchUser(parseInt(params.id!));
     if (!issue) notFound();
     return (
         <Grid columns={{ initial: "1", sm: "5" }} gap="5">
@@ -47,9 +49,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 }
 
 export async function generateMetadata({params}:Props) {
-    const issue=await prisma.issue.findUnique({
-        where:{id:parseInt(params.id!)}
-    })
+    const issue=await fetchUser(parseInt(params.id!));
 
     return {
         title:issue?.title,
